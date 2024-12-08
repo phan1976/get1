9fd537108dc368ac746e5afe669637e4639db0f7
vtp5335 <vtp5335@gmail.com>
2024-12-08 22:53:53 +0100

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GetDataComponent } from './get-data/get-data.component';
+import { PostDataComponent } from './post-data/post-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    GetDataComponent,
+    PostDataComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/get-data/get-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-data',
+  templateUrl: './get-data.component.html',
+  styleUrls: ['./get-data.component.scss']
+})
+export class GetDataComponent {
+
+}
